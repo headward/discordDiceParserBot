5a52b25ea5607f7e06765d395cd08001eab91be5
Kevin Fégar <kevin.fegar@gmail.com>
2022-01-04 18:07:58 +0100

    Make improvements on error handling and messages, add help command

--- diceParser.js
+++ diceParser.js
@@ -1,21 +1,39 @@
 import { PREFIX } from "./init.js";
 
 export default {
+    errorMessage: 'tu as du te tromper dans ton jet de dé, il manque des paramètres \n tape `!aide` pour plus d\'informations',
+    helpMessage: 'Pour lancer un/des dé(s) voici comment faire :\n tape ton nombre de dé(s) suivi par le nombre de faces \nexemple `2d20` pour lancer 2 dés à 20 faces',
     init(client) {
         client.on('messageCreate', async message =>{
 			if(message.content.startsWith(PREFIX)){
                 const input = message.content.slice(PREFIX.length);
+                if(this.isHelp(input)){
+                    message.reply(this.helpMessage);
+                    message.delete();
+                    return
+                }
+
+                if(!this.hasNumber(input)) {
+                    message.reply(this.errorMessage);
+                    message.delete();
+                    return;
+                }
+
                 const dices = this.getDices(input);
                 const faces = this.getFaces(input);
                 if(dices.count === 0|| faces.count === 0){
-                    message.reply('tu as du te tromper dans ton jet de dé')
+                    message.reply(this.errorMessage)
+                    message.delete();
                     return;
                 }
                 let roll = '';
                 roll = this.rollTheDices(dices.count, faces.count);
-                message.reply('`'+dices.count + ' dé(s) à '+ faces.count+'faces, résultat :\n' + roll+'`')
+                message.reply(dices.count + ' dé(s) à '+ faces.count+' faces, résultat : \n `' + roll+'`')
 			}
 		})
+    },
+    isHelp(input) {
+        return input === 'aide';
     },
 
     getDices(input) {
@@ -53,7 +71,9 @@ export default {
         for(let i = 0; dicesCount > i; i++) {
             roll += Math.floor(Math.random() * (facesCount - min +1)) + min + ' ';
         }
-        console.log(roll);
         return roll
+    },
+    hasNumber(input) {
+        return /\d/.test(input);
     }
 };
